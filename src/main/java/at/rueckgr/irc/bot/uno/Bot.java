4f164046da145e6ee6c4b62b82cbcdbc584176ce
Paul Staroch <paulchen@rueckgr.at>
2015-07-13 18:51:42 +0200

    added TODOs

--- src/main/java/at/rueckgr/irc/bot/uno/Bot.java
+++ src/main/java/at/rueckgr/irc/bot/uno/Bot.java
@@ -11,8 +11,10 @@ import java.util.Map;
 
 public class Bot extends PircBot {
 
+    // TODO use name reported by PircBot
     public static final String NAME = "unobot";
 
+    // TODO use properties
     private static final String NETWORK = "irc.rueckgr.at";
     private static final String CHANNEL = "#uno";
     private static final String BOT_NAME = "GamingPrincessLuna";
