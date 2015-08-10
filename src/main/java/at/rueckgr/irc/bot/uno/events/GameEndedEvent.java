3353b1a7e0f2087099506ae3971f627a39033874
Paul Staroch <paulchen@rueckgr.at>
2015-08-10 20:43:45 +0200

    only use !elo at the end of the game with a certain probability

--- src/main/java/at/rueckgr/irc/bot/uno/events/GameEndedEvent.java
+++ src/main/java/at/rueckgr/irc/bot/uno/events/GameEndedEvent.java
@@ -9,11 +9,15 @@ import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 @SuppressWarnings("unused")
 public class GameEndedEvent implements Event {
     private static final Logger logger = LoggerFactory.getLogger(GameEndedEvent.class);
 
     private static final String COMMAND = "game_ended";
+
+    private final Random random = new Random();
 
     @Override
     public Action handle(UnoState unoState, JSONObject object, BotInfoProvider botInfoProvider) {
@@ -21,7 +25,10 @@ public class GameEndedEvent implements Event {
 
         logger.debug("Game ended");
 
-        return new ChannelMessageAction("!elo");
+        if(random.nextInt(10) > 8) {
+            return new ChannelMessageAction("!elo");
+        }
+        return null;
     }
 
     @Override
