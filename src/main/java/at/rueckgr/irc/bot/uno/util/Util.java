0641272a34e0148665db4dd14d0742a23aad260b
Paul Staroch <paulchen@rueckgr.at>
2018-08-23 21:22:04 +0200

    when joining, check if GamingPrincessLuna is in the channel; code cleanup and refactoring

--- src/main/java/at/rueckgr/irc/bot/uno/util/Util.java
+++ src/main/java/at/rueckgr/irc/bot/uno/util/Util.java
@@ -5,27 +5,42 @@ import at.rueckgr.irc.bot.uno.actions.ChannelMessageAction;
 import at.rueckgr.irc.bot.uno.actions.WaitAction;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Util {
+    private static final int WAIT_TIME = 1000;
+
+    public static List<Action> createAutoplayCommands(UnoMode... unoModes) {
+        final String unoOptions = Arrays.stream(unoModes)
+                .map(UnoMode::getCode)
+                .collect(Collectors.joining(" "));
+
+        return createAutoplayCommands(unoOptions);
+    }
+
     public static List<Action> createAutoplayCommands(String unoOptions) {
         List<Action> result = new ArrayList<>();
 
-        result.add(new ChannelMessageAction("!uno " + unoOptions));
+        result.add(new ChannelMessageAction(UnoCommands.START_GAME + " " + unoOptions));
 
-        result.add(new ChannelMessageAction("?join"));
-        result.add(new WaitAction(1000));
+        result.add(new ChannelMessageAction(UnoCommands.INVITE_BOT));
+        result.add(new WaitAction(WAIT_TIME));
 
-        result.add(new ChannelMessageAction("!leave"));
-        result.add(new WaitAction(1000));
+        result.add(new ChannelMessageAction(UnoCommands.LEAVE_GAME));
+        result.add(new WaitAction(WAIT_TIME));
 
-        result.add(new ChannelMessageAction("!botjoin"));
-        result.add(new WaitAction(1000));
+        result.add(new ChannelMessageAction(UnoCommands.BOTJOIN));
+        result.add(new WaitAction(WAIT_TIME));
 
-        result.add(new ChannelMessageAction("!deal"));
-
-
+        result.add(new ChannelMessageAction(UnoCommands.DEAL));
 
         return result;
+    }
+
+    public static List<Action> createInvitePrincessCommands() {
+        return Collections.singletonList(new ChannelMessageAction(UnoCommands.INVITE_PRINCESS));
     }
 }
